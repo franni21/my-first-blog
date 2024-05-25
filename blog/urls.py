9b1578d74842fa193ecd81e9fd99beeb21752db5
franni21 <pscfnc@gmail.com>
2024-05-25 12:15:21 +0200

    css, list_view e detail_view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+
 ]
