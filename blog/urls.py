61ef3f5077af5ebc0cf0158629df778c708b0fb8
franni21 <pscfnc@gmail.com>
2024-05-25 12:51:31 +0200

    new post edit post solo con utente loggato

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 
 ]
